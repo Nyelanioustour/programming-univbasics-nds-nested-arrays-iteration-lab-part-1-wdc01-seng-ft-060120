f470e003e22433738a0719d11ef05e102e24afcf
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-24 19:26:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def find_even_values(src)
   output1 = []
   output2 = []
@@ -22,6 +23,7 @@ def find_even_values(src)
     count +=1
   end
   output2
+  binding.pry
 end 
   
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
